d306a0d9ecc14c4c2c5fc1580aa3be225583f202
Wallace Silva <wrslogan@outlook.com>
2021-11-14 16:04:31 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,9 @@
 package application;
 
-import entities.Department;
-
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		
-		System.out.println(obj);
 
 	}
 
