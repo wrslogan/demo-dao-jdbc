729fdbc8b9a1aeef89b758b09c219dd78aa7e5fd
Wallace Silva <wrslogan@outlook.com>
2021-11-15 09:44:13 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,17 +25,22 @@ public class Program {
 			System.out.println(obj);
 		}
 
-		
 		System.out.println("\n=== TEST 3: seller findAll ===");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n=== TEST 4: seller insert ===");
-		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(), 4000.0, dep);
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 
 	}
 
